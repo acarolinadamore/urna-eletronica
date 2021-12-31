53f14c52c018c50db5563659f6dacc15906dedef
Ana <acarolinadamore@gmail.com>
2021-12-31 00:21:41 -0400

    Script - Iniciado com lista de array das etapas

---
+++ etapas.js
@@ -0,0 +1,42 @@
+let etapas = {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: 10,
+            name: 'Han Solo',
+            partido: 'Falcon',
+            fotos: [
+                {url:'hansolo.png', legenda:'Prefeito', small:false}
+            ]
+        },
+        {
+            numero: 20,
+            name: 'Luke',
+            partido: 'Skywalker',
+            fotos: [
+                {url:'luke.png', legenda:'Prefeito', small:false}
+            ]
+        }
+    ],
+    titulo: 'VICE',
+    numeros: 4,
+    candidatos: [
+        {
+            numero: 10,
+            name: 'Chewie',
+            partido: 'Falcon',
+            fotos: [
+                {url:'chewie.png', legenda:'Vice', small:false}
+            ]
+        },
+        {
+            numero: 10,
+            name: 'Leia',
+            partido: 'Falcon',
+            fotos: [
+                {url:'chewie.png', legenda:'Vice', small:false}
+            ]
+        }
+    ]
+},
